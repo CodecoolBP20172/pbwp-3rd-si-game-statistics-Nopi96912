9cc526a9a7beaa5a66d0caf582621763d37f4cb2
nopi96912 <nopiwork@gmail.com>
2017-06-25 17:57:45 +0200

    decide function working in the test file

--- reports.py
+++ reports.py
@@ -1,3 +1,10 @@
 def count_games(file_name='game_stat.txt'):
     with open(file_name, 'r') as source_file:
         return sum(1 for line in source_file)
+
+
+def decide(file_name, year):
+    if str(year) in open(file_name).read():
+        return True
+    else:
+        return False
