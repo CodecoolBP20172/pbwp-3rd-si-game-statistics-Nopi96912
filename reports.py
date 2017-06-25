26802fae7e0a583d5213fa337b47c50949e3a11a
nopi96912 <nopiwork@gmail.com>
2017-06-25 20:50:51 +0200

    get line number by title working in the test file

--- reports.py
+++ reports.py
@@ -36,3 +36,16 @@ def count_by_genre(file_name, genre):
         counter += 1
     genre_dict = dict((i, genre_list.count(i)) for i in genre_list)
     return genre_dict.get(genre)
+
+
+def get_line_number_by_title(file_name, title):
+    data = []
+    with open(file_name, 'r') as source_file:
+        for line in source_file:
+            data.append(line.strip().split('\t'))
+    title_list = []
+    counter = 0
+    for item in data:
+        title_list.append(data[counter][0])
+        counter += 1
+    return int(title_list.index(title)) + 1
