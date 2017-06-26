8d7f15b688679816244f4eccf5bb0d11a079715e
nopi96912 <nopiwork@gmail.com>
2017-06-26 20:15:24 +0200

    Printing with pprint

--- part2/printing.py
+++ part2/printing.py
@@ -1,2 +1,13 @@
+import pprint
+from reports import *
 
-# Printing functions
+
+pp = pprint.PrettyPrinter(width=80, compact=True)
+pp.pprint(get_most_played("game_stat.txt"))
+pp.pprint(sum_sold("game_stat.txt"))
+pp.pprint(get_selling_avg("game_stat.txt"))
+pp.pprint(count_longest_title("game_stat.txt"))
+pp.pprint(get_date_avg("game_stat.txt"))
+pp.pprint(get_game("game_stat.txt", 'Minecraft'))
+pp.pprint(count_grouped_by_genre("game_stat.txt"))
+pp.pprint(get_date_ordered("game_stat.txt"))
