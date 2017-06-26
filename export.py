d671c8dd62ca0a9193836079a9e81eb1ac3e4811
nopi96912 <nopiwork@gmail.com>
2017-06-26 21:06:55 +0200

    Correcting function

--- export.py
+++ export.py
@@ -1,15 +1,15 @@
 from reports import *
 
 
-def export_to_file(file_name):
+def export_to_file(file_name, source_file):
     with open(file_name, 'w', newline='\n') as export:
-        export.writelines(str(count_games("game_stat.txt"))+"\n")
-        export.writelines(str(decide("game_stat.txt", 1998))+"\n")
-        export.writelines(str(get_latest("game_stat.txt"))+"\n")
-        export.writelines(str(count_by_genre("game_stat.txt", "RPG"))+"\n")
-        export.writelines(str(get_line_number_by_title("game_stat.txt", "Command & Conquer"))+"\n")
-        export.writelines(str(sort_abc("game_stat.txt"))+"\n")
-        export.writelines(str(get_genres("game_stat.txt"))+"\n")
-        export.writelines(str(when_was_top_sold_fps("game_stat.txt")))
+        export.writelines(str(count_games(source_file))+"\n")
+        export.writelines(str(decide(source_file, 1998))+"\n")
+        export.writelines(str(get_latest(source_file))+"\n")
+        export.writelines(str(count_by_genre(source_file, "RPG"))+"\n")
+        export.writelines(str(get_line_number_by_title(source_file, "Command & Conquer"))+"\n")
+        export.writelines(str(sort_abc(source_file))+"\n")
+        export.writelines(str(get_genres(source_file))+"\n")
+        export.writelines(str(when_was_top_sold_fps(source_file)))
 
-export_to_file('export.txt')
+export_to_file('export.txt', 'game_stat.txt')
