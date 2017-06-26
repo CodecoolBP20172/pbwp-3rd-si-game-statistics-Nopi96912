9a0b80fe18d1e9931e6b0527b1e0576099263f59
nopi96912 <nopiwork@gmail.com>
2017-06-26 12:01:39 +0200

    Using pprint for clear output

--- printing.py
+++ printing.py
@@ -1,2 +1,13 @@
+import pprint
+from reports import *
 
-# Printing functions
+
+pp = pprint.PrettyPrinter(width=80, compact=True)
+pp.pprint(count_games("game_stat.txt"))
+pp.pprint(decide("game_stat.txt", "2009"))
+pp.pprint(get_latest("game_stat.txt"))
+pp.pprint(count_by_genre("game_stat.txt", "RPG"))
+pp.pprint(get_line_number_by_title("game_stat.txt", "Command & Conquer"))
+pp.pprint(sort_abc("game_stat.txt"))
+pp.pprint(get_genres("game_stat.txt"))
+pp.pprint(when_was_top_sold_fps("game_stat.txt"))
