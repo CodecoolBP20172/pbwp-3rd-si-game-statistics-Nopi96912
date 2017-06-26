40540b84a80bfce4cab06d1b2c9cc40048c02f3f
nopi96912 <nopiwork@gmail.com>
2017-06-26 20:18:33 +0200

    export works fine

--- part2/export.py
+++ part2/export.py
@@ -1,2 +1,15 @@
+from reports import *
 
-# Export functions
+
+def export_to_file(file_name):
+    with open(file_name, 'w', newline='\n') as export:
+        export.writelines(str(get_most_played("game_stat.txt"))+"\n")
+        export.writelines(str(sum_sold("game_stat.txt"))+"\n")
+        export.writelines(str(get_selling_avg("game_stat.txt"))+"\n")
+        export.writelines(str(count_longest_title("game_stat.txt"))+"\n")
+        export.writelines(str(get_date_avg("game_stat.txt"))+"\n")
+        export.writelines(str(get_game("game_stat.txt", 'Minecraft'))+"\n")
+        export.writelines(str(count_grouped_by_genre("game_stat.txt"))+"\n")
+        export.writelines(str(get_date_ordered("game_stat.txt")))
+
+export_to_file('export.txt')
