8d1107c89784f99fcf2d88083a13161c56872799
nopi96912 <nopiwork@gmail.com>
2017-06-26 21:05:33 +0200

    Correcting function

--- part2/export.py
+++ part2/export.py
@@ -1,15 +1,15 @@
 from reports import *
 
 
-def export_to_file(file_name):
+def export_to_file(file_name, source_file):
     with open(file_name, 'w', newline='\n') as export:
-        export.writelines(str(get_most_played("game_stat.txt"))+"\n")
-        export.writelines(str(sum_sold("game_stat.txt"))+"\n")
-        export.writelines(str(get_selling_avg("game_stat.txt"))+"\n")
-        export.writelines(str(count_longest_title("game_stat.txt"))+"\n")
-        export.writelines(str(get_date_avg("game_stat.txt"))+"\n")
-        export.writelines(str(get_game("game_stat.txt", 'Minecraft'))+"\n")
-        export.writelines(str(count_grouped_by_genre("game_stat.txt"))+"\n")
-        export.writelines(str(get_date_ordered("game_stat.txt")))
+        export.writelines(str(get_most_played(source_file))+"\n")
+        export.writelines(str(sum_sold(source_file))+"\n")
+        export.writelines(str(get_selling_avg(source_file))+"\n")
+        export.writelines(str(count_longest_title(source_file))+"\n")
+        export.writelines(str(get_date_avg(source_file))+"\n")
+        export.writelines(str(get_game(source_file, 'Minecraft'))+"\n")
+        export.writelines(str(count_grouped_by_genre(source_file))+"\n")
+        export.writelines(str(get_date_ordered(source_file)))
 
-export_to_file('export.txt')
+export_to_file('export.txt', 'game_stat.txt')
