a745d85a8d1aa30e6f33e4957a4368b17cece58a
nopi96912 <nopiwork@gmail.com>
2017-06-26 18:02:20 +0200

    All basic functions working in the test file

--- part2/reports.py
+++ part2/reports.py
@@ -1,3 +1,6 @@
+import math
+
+
 def list_from_file(file_name, place, type):
     data = []
     with open(file_name, 'r') as source_file:  # make a 2D list from the file, each line is one list
@@ -24,3 +27,25 @@ def sum_sold(file_name):
 def get_selling_avg(file_name):
     data, temp, counter = list_from_file(file_name, 1, float)
     return (sum(temp) / len(temp))
+
+
+def count_longest_title(file_name):
+    data, temp, counter = list_from_file(file_name, 0, str)
+    return int(len(max(temp, key=len)))
+
+
+def get_date_avg(file_name):
+    data, temp, counter = list_from_file(file_name, 2, float)
+    return int(math.ceil(sum(temp) / len(temp)))
+
+
+def get_game(file_name, title):
+    data, temp, counter = list_from_file(file_name, 0, str)
+    place = 0
+    for item in temp:
+        if item == title:
+            new_list = data[place]
+        place += 1
+    new_list[1] = float(new_list[1])
+    new_list[2] = int(new_list[2])
+    return new_list
